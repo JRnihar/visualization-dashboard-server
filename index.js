a2aa31b5dba0507acc9e4773d8ef6ee039579a0b
JRnihar <niharmonirulislam@gmail.com>
2022-06-05 22:51:09 +0600

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion} = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
